2f81a9d0374c534a522449a4baf9ae068f1a649d
nexora-basset <veramuna2000@gmail.com>
2026-07-07 14:53:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function toggleMenu() {
-    document.querySelector(".dropdown-content").classList.toggle("show");
+// Menu mobile
+
+const menu=document.getElementById("menu-toggle");
+
+const nav=document.getElementById("navbar");
+
+menu.onclick=()=>{
+
+nav.classList.toggle("active");
+
+}
+
+// Header au scroll
+
+window.addEventListener("scroll",()=>{
+
+const header=document.querySelector(".header");
+
+header.classList.toggle("scrolled",window.scrollY>50);
+
+});
 }
