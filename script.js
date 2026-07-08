6de1ae0dc71d791668bd68b1db756d2623c0ce22
nexora-basset <veramuna2000@gmail.com>
2026-07-08 11:18:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
 
-// NEXORA TRADE & LOGISTICS
-// Script du site
-
-// Message de confirmation du formulaire
-
-document.querySelector("form").addEventListener("submit", function(event) {
-
-    event.preventDefault();
-
-    alert("Merci pour votre demande. NEXORA TRADE & LOGISTICS vous contactera rapidement.");
-
-});
+/
