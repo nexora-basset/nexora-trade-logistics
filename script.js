502cc48081eeb71c2074665fedffdcf0ce4f2e67
nexora-basset <veramuna2000@gmail.com>
2026-07-10 14:20:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -75,3 +75,13 @@ if(footer){
 
 }
 /
+const menuToggle = document.getElementById("menu-toggle");
+
+const navLinks = document.getElementById("nav-links");
+
+
+menuToggle.addEventListener("click",()=>{
+
+    navLinks.classList.toggle("active");
+
+});
